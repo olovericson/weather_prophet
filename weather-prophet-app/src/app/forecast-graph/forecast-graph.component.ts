fed3b91bad19cb5e434df7212d77b796c2d9465b
olovericson <olov_ericson@hotmail.com>
2018-06-08 16:00:01 +0200

    A first plotly graph

--- weather-prophet-app/src/app/forecast-graph/forecast-graph.component.ts
+++ weather-prophet-app/src/app/forecast-graph/forecast-graph.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Plotly from 'plotly.js';
+import {Config, Data, Layout} from 'plotly.js';
 
 @Component({
   selector: 'app-forecast-graph',
@@ -9,6 +11,13 @@ export class ForecastGraphComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    Plotly.plot('plotlyDiv',
+      [{
+              x: [1, 2, 3, 4, 5],
+              y: [1, 2, 4, 8, 16]
+            }],
+      { margin: { t: 0 } }
+      );
   }
 
 }
